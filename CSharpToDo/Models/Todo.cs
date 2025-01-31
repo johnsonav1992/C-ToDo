5b16a58edbdafdad20a89bb75b3f8dc3cadc016f
Alex Johnson <johnsonav1992@gmail.com>
2025-01-30 20:46:42 -0600

    Initialize CreatedAt and CompletedAt properties in Todo model with default values

--- CSharpToDo/Models/Todo.cs
+++ CSharpToDo/Models/Todo.cs
@@ -5,8 +5,8 @@ namespace CSharpToDo.Models
         public int Id { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
-        public bool IsCompleted { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? CompletedAt { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? CompletedAt { get; set; } = null;
     }
 }
